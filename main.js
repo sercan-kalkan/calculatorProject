b10b315e2f9667c1d3bcc6ab45b7eba3d47d9b8f
“Sercan <sercankalkan1@gmail.com>
2023-08-06 14:48:56 +0300

    click event added to buttons

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let firstNumber;
 let secondNumber;
 let operator;
+
+
 function add(a,b) {
     return a+b;
 }
@@ -20,3 +22,13 @@ function divide (a,b) {
 function operate (operator, firstNumber, secondNumber) {
 
 }
+
+
+let btns = document.querySelectorAll('button');
+let input = document.querySelector('input');
+btns.forEach(function (i) {
+  i.addEventListener('click', function(e) {
+    input.placeholder += i.value;
+});
+});
+
